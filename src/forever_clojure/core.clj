24ea1d9ebaf3c9672eb75f0557a44d57e12ab0ca
knikoletic <karlo.nikoletic@verybigthings.com>
2021-12-20 10:56:41 +0100

    Simple Math

--- src/forever_clojure/core.clj
+++ src/forever_clojure/core.clj
@@ -4,3 +4,7 @@
 ;; [Problem 1: Nothing but the Truth [Elementary]](http://www.4clojure.com/problem/1)
 
 (= true true)
+
+;; [Problem 2: 	Simple Math [Elementary]](http://www.4clojure.com/problem/2)
+
+(= (- 10 (* 2 3)) 4)
